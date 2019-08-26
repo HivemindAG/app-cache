d1ea42ae640d9a46123eec7e0d6ee85edc9bfb6e
Slobodan Jovanović <slobodan@hivemind.ch>
2019-08-26 14:53:28 +0200

    bug in preparing request for direct api access with call() function. SMAR-234

--- src/api-request.js
+++ src/api-request.js
@@ -8,7 +8,7 @@ function mkOptions(session, args) {
     method: 'GET',
   };
   if (typeof args === 'string') {
-    args = {path: args};
+    args = { path: args };
   }
   Object.assign(options, args);
   options.headers = Object.assign({}, args.headers);
@@ -16,7 +16,7 @@ function mkOptions(session, args) {
     options.headers['API-Key'] = session.apiKey;
   }
   if (typeof options.path === 'string') {
-    options.url = session.apiURL + options.path;
+    options.url = config.apiURL + options.path;
     delete options.path;
   }
   return options;
@@ -24,7 +24,7 @@ function mkOptions(session, args) {
 
 function call(session, args, cbk) {
   const options = mkOptions(session, args);
-  callQueue.push({options, cbk});
+  callQueue.push({ options, cbk });
   dispatch();
 }
 
@@ -40,7 +40,7 @@ function dispatch() {
     nInFlight -= 1;
     dispatch();
     const cbk = args.cbk;
-    if (err) console.error('REQERR: ' + JSON.stringify({req: args.options, err}));
+    if (err) console.error('REQERR: ' + JSON.stringify({ req: args.options, err }));
     if (err) return cbk(err, res);
     const ans = utils.optParse(body, undefined);
     if (res.statusCode < 200 || res.statusCode >= 300) {
